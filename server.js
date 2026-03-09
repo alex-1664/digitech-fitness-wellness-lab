cc0ac4e82bf6aa6e0c4a784dc371ebc0ca98f7ad
alex-1664 <aleckiekiio@gmail.com>
2026-03-09 11:01:07 -0700

    Add user login functionality
    Implement login route with user authentication using bcrypt.

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
-app.get("/", (req,res)=>{
-res.sendFile(__dirname + "/public/login.html")
+const users = require("./utils/users")
+const bcrypt = require("bcrypt")
+
+app.use(express.json())
+
+app.post("/login", async (req,res)=>{
+  const {username, password} = req.body
+
+  const user = users.find(u => u.username === username)
+  if(!user){
+    return res.json({success:false, message:"User not found"})
+  }
+
+  const valid = await bcrypt.compare(password, user.password)
+  if(valid){
+    return res.json({success:true, message:"Login successful"})
+  }else{
+    return res.json({success:false, message:"Wrong password"})
+  }
 })
