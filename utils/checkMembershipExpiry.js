730dda40e439fd9721c8932dac7fc824225b81e0
alex-1664 <aleckiekiio@gmail.com>
2026-03-09 23:29:37 -0700

    Update checkMembershipExpiry.js

--- utils/checkMembershipExpiry.js
+++ utils/checkMembershipExpiry.js
@@ -1,23 +1,15 @@
-const sendSMS = require("./renewalReminder")
+const sendSMS = require("./sms")
 
 function checkMembershipExpiry(members){
-
-const today = new Date()
-
-members.forEach(member => {
-
-const expiry = new Date(member.membershipExpiry)
-
-const daysLeft = (expiry - today) / (1000*60*60*24)
-
-if(daysLeft <= 3 && daysLeft > 0){
-
-sendSMS(member.phone, member.name)
-
-}
-
-})
-
+  const today = new Date()
+  members.forEach(member=>{
+    const expiry = new Date(member.membershipExpiry)
+    const daysLeft = (expiry - today)/(1000*60*60*24)
+    if(daysLeft <=3 && daysLeft>0){
+      const msg = `Hello ${member.name}, your membership expires in ${Math.ceil(daysLeft)} day(s). Renew via M-PESA today!`
+      sendSMS(member.phone, msg)
+    }
+  })
 }
 
 module.exports = checkMembershipExpiry
