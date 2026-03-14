f977f2bfe971c9eb975b60b929b30683d9c52920
ALEX <aleckiekiio@gmail.com>
2026-03-14 14:42:24 +0300

    sms.js

--- sms.js
+++ sms.js
@@ -1,33 +1,17 @@
-const axios = require("axios");
+const axios = require("axios")
+const { CONSUMER_KEY, CONSUMER_SECRET, SHORTCODE } = process.env
 
-async function sendSMS(phone, message) {
-    try {
-        // Safaricom Daraja SMS endpoint (for sandbox)
-        const smsUrl = "https://sandbox.safaricom.co.ke/mpesa/sms/v1/send";
-
-        // Access Token (same as MPESA access token)
-        const token = await getAccessToken(); // use your existing function
-
-        // Make POST request
-        const response = await axios.post(
-            smsUrl,
-            {
-                ShortCode: process.env.SHORTCODE,  // your shortcode
-                Message: message,
-                Msisdn: phone
-            },
-            {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                    "Content-Type": "application/json"
-                }
-            }
-        );
-
-        console.log("SMS sent:", response.data);
-    } catch (err) {
-        console.log("SMS failed:", err.message);
-    }
+async function getAccessToken() {
+  const auth = Buffer.from(`${CONSUMER_KEY}:${CONSUMER_SECRET}`).toString("base64")
+  const url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+  const res = await axios.get(url, { headers: { Authorization: `Basic ${auth}` } })
+  return res.data.access_token
 }
 
-module.exports = { sendSMS };
+async function sendSMS(phone, message){
+  const token = await getAccessToken()
+  // Use Safaricom API (C2B) or a simulated SMS endpoint
+  console.log(`Sending SMS to ${phone}: ${message}`)
+}
+
+module.exports = sendSMS
