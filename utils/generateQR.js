d2210adc7accc01f5c55f1cb0bad273a577b7de2
ALEX <aleckiekiio@gmail.com>
2026-03-14 21:16:29 -0700

    generateQR.js

--- utils/generateQR.js
+++ utils/generateQR.js
@@ -1,10 +1,20 @@
 const QRCode = require("qrcode")
 const path = require("path")
+const fs = require("fs")
+
+const folder = path.join(__dirname,"../public/qrcodes")
+
+if(!fs.existsSync(folder)){
+  fs.mkdirSync(folder)
+}
 
 async function generateQR(memberId){
-  const qrPath = path.join(__dirname,"../public/qrcodes",memberId + ".png")
-  await QRCode.toFile(qrPath, memberId)
-  return "/qrcodes/" + memberId + ".png"
+
+  const file = path.join(folder,`member-${memberId}.png`)
+
+  await QRCode.toFile(file,String(memberId))
+
+  return `/qrcodes/member-${memberId}.png`
 }
 
 module.exports = generateQR
